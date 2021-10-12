ece3b5a2de5976c0cd83d447920283ba68ddf20d
John Baik <johnwbaik@gmail.com>
2021-10-12 11:11:41 -0700

    Completed javascript-data-models exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,83 @@
+var orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    total: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    orderDetails: 'Order Details',
+    Invoice: 'Invoice',
+    dateDelivered: 'Aug 8, 2020',
+    packageConfirmation: false,
+    orderTitle: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnWindow: 'Sep 7, 2020',
+    price: 31.55,
+    repeatPurchase: 'Buy it again',
+    viewItem: 'View your item',
+    review: 'Write a product review'
+  },
+  {
+    orderDate: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    orderDetails: 'Order Details',
+    Invoice: 'Invoice',
+    dateDelivered: 'Jul 20, 2020',
+    packageConfirmation: true,
+    orderTitle: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnWindow: 'Aug 19, 2020',
+    price: 41.33,
+    repeatPurchase: 'Buy it again',
+    viewItem: 'View your item',
+    review: 'Write a product review'
+  },
+  {
+    orderDate: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    orderDetails: 'Order Details',
+    Invoice: 'Invoice',
+    dateDelivered: 'Jul 7, 2020',
+    packageConfirmation: true,
+    orderTitle: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for WII U, PC, Support Turbo and Vibration Features, No Driver and No Lag-Gamecube Adapter',
+    returnWindow: 'Aug 5, 2020',
+    price: 15.98,
+    repeatPurchase: 'Buy it again',
+    viewItem: 'View your item',
+    review: 'Write a product review'
+  },
+  {
+    orderDate: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    orderDetails: 'Order Details',
+    Invoice: 'Invoice',
+    dateDelivered: 'Jul 5, 2020',
+    review: 'Write a product review',
+    items: [
+      {
+        packageConfirmation: false,
+        orderTitle: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch',
+        returnWindow: 'Aug 5, 2020',
+        price: 94.95,
+        repeatPurchase: 'Buy it again',
+        viewItem: 'View your item'
+      },
+      {
+        packageConfirmation: false,
+        orderTitle: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindow: 'Aug 4, 2020',
+        price: 33.99,
+        repeatPurchase: 'Buy it again',
+        viewItem: 'View your item'
+      }
+    ]
+  }
+];
+
+console.log(orderHistory);
