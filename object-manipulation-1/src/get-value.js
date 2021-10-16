cef435eb33b2232262677a2d46c66fa7d3fecdc7
John Baik <johnwbaik@gmail.com>
2021-10-15 22:01:45 -0700

    Completed object-manipulation-1 exercise.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,7 @@
+/* exported getValue */
+function getValue(object, key) {
+  return object[key];
+}
+
+// define function getValue with 2 parameters, object and key
+// return value in object with index of key.
