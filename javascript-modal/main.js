695f57d4dae56bb1722ede83f1c246225b36331b
John Baik <johnwbaik@gmail.com>
2021-10-19 16:03:33 -0700

    Completed javascript-modal exercise UPDATE

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -4,20 +4,12 @@ var container = document.querySelector('.container-none');
 modal.addEventListener('click', ad);
 
 function ad(event) {
-  var i = 0;
-  if (i % 2 === 0) {
-    container.className = 'container-ad';
-  } else {
-    container.className = 'container-none';
-  }
+  container.className = 'container-ad';
 }
 
 var no = document.querySelector('.no');
 no.addEventListener('click', negative);
 
 function negative(event) {
-  var i = 0;
-  if (i % 2 === 0) {
-    container.className = 'container-none';
-  }
+  container.className = 'container-none';
 }
