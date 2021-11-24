69ec1e4440eddeeb225d5f94459828f763b398e5
John Baik <johnwbaik@gmail.com>
2021-11-24 12:47:24 -0800

    Completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('typeof:', typeof exports);
+console.log('value:', exports);
+
+console.log('typeof:', typeof require);
+console.log('value:', require);
+
+console.log('typeof:', typeof module);
+console.log('value:', module);
+
+console.log('typeof:', typeof __filename);
+console.log('value:', __filename);
+
+console.log('typeof:', typeof __dirname);
+console.log('value:', __dirname);
