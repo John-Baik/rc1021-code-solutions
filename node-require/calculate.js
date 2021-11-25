c57b5edb1c2632a0ea62336d211fefa1d6040fa2
John Baik <johnwbaik@gmail.com>
2021-11-24 17:28:14 -0800

    Updated code to use ||

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -7,12 +7,12 @@ const op = process.argv[3].toLowerCase();
 const num1 = parseFloat(process.argv[2]);
 const num2 = parseFloat(process.argv[4]);
 
-if (op === 'plus' | op === '+') {
+if (op === 'plus' || op === '+') {
   add(num1, num2);
-} else if (op === 'minus' | op === '-') {
+} else if (op === 'minus' || op === '-') {
   subtract(num1, num2);
-} else if (op === 'times' | op === 'x' | op === '*') {
+} else if (op === 'times' || op === 'x' || op === '*') {
   multiply(num1, num2);
-} else if (op === 'divided' | op === '/' | op === 'over') {
+} else if (op === 'divided' || op === '/' || op === 'over') {
   divide(num1, num2);
 }
