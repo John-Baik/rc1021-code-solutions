be849b3e1a6d1e63b01e64ab9b91734ecb8418d3
John Baik <johnwbaik@gmail.com>
2021-12-03 15:47:46 -0800

    UPDATED

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -15,7 +15,7 @@ app.get('/api/notes', (req, res) => {
 
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
-  if (!id || id < 0) {
+  if (!id || id < 0 || Number.isInteger(id) === false) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (!notes[id]) {
     res.status(404).json({ error: `cannot find note with id ${id}` });
