3cb9b9a2a56eed3a624b94d43fad6fcfb5f3a9b4
John Baik <johnwbaik@gmail.com>
2021-12-14 18:46:11 -0800

    Completed react-validated-input-component

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Password from './validated-input';
+
+ReactDOM.render(<Password />, document.getElementById('root'));
---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class Password extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: false,
+      password: '',
+      message: 'A password is required'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    this.setState({ password: value });
+    const password = event.target.value;
+    const length = password.length;
+    if (length === 0) {
+      this.setState({
+        status: false,
+        message: 'A password is required'
+      });
+    } else if (length >= 8) {
+      this.setState({
+        status: true,
+        message: ''
+      });
+    } else if (length < 8 && length > 0) {
+      this.setState({
+        status: false,
+        message: 'password is too short'
+      });
+    }
+  }
+
+  render() {
+    return (
+      <form>
+      <div className="container">
+        <label className="title" htmlFor="password">Password</label>
+        <div className="box">
+          <input onChange={this.handleChange} type="text" name="password" id="password"></input>
+            <i className={this.state.status ? 'fas fa-check' : 'fas fa-times'}></i>
+        </div>
+        <div className="notification">
+          <p className="message">{this.state.message}</p>
+        </div>
+      </div>
+    </form>
+    );
+  }
+}
+
+export default Password;
